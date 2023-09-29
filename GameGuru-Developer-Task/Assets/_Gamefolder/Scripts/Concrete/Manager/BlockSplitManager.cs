025a00e1f23019547e39d590953817603e6e40b7
Strife915 <thekarabulut95@gmail.com>
2023-09-30 02:01:15 +0300

    feat:Block split in progress

--- GameGuru-Developer-Task/Assets/_Gamefolder/Scripts/Concrete/Manager/BlockSplitManager.cs
+++ GameGuru-Developer-Task/Assets/_Gamefolder/Scripts/Concrete/Manager/BlockSplitManager.cs
@@ -8,17 +8,34 @@ public class BlockSplitManager : IBlockSplitManager
 
     public void CalculateForfeit()
     {
-        float hangover = MovingBlock.position.x - LastBlock.position.x;
-        SplitBlock(hangover);
+        SplitBlock();
     }
 
-    void SplitBlock(float hangover)
+    void SplitBlock()
     {
-        float newSize = LastBlock.localScale.x - Mathf.Abs(hangover);
-        float fallingBlockSize = MovingBlock.localScale.x - newSize;
+        Vector3 size1 = LastBlock.GetComponentInChildren<Renderer>().bounds.size;
+        Vector3 size2 = MovingBlock.GetComponentInChildren<Renderer>().bounds.size;
 
-        float newXPosition = LastBlock.position.x + (hangover / 2);
-        MovingBlock.localScale = new Vector3(newSize, MovingBlock.localScale.y, MovingBlock.localScale.z);
-        MovingBlock.position = new Vector3(newXPosition, MovingBlock.position.y, MovingBlock.position.z);
+        Vector3 position1 = LastBlock.transform.position;
+        Vector3 position2 = MovingBlock.transform.position;
+
+        float overflowAmount = (position2.x + (size2.x / 2)) - (position1.x + (size1.x / 2));
+
+        Debug.Log("Overflow Amount: " + overflowAmount);
+
+        Vector3 newScale2 = MovingBlock.transform.localScale - new Vector3(Mathf.Abs(overflowAmount), 0, 0);
+        MovingBlock.transform.localScale = newScale2;
+
+        float newPositionX = position1.x;
+        if (overflowAmount > 0)
+        {
+            newPositionX += (size1.x / 2) + (newScale2.x / 2);
+        }
+        else
+        {
+            newPositionX -= (size1.x / 2) + (newScale2.x / 2);
+        }
+
+        MovingBlock.transform.position = new Vector3(newPositionX / 2, position2.y, position2.z);
     }
 }
