db58e70ef4f03095d9f02c138a146d4602c675d8
Strife915 <thekarabulut95@gmail.com>
2023-10-01 00:56:46 +0300

    feat: Split block implemented

--- GameGuru-Developer-Task/Assets/_Gamefolder/Scripts/Concrete/Manager/BlockSplitManager.cs
+++ GameGuru-Developer-Task/Assets/_Gamefolder/Scripts/Concrete/Manager/BlockSplitManager.cs
@@ -10,41 +10,50 @@ public class BlockSplitManager : IBlockSplitManager
 
     public void CalculateForfeit()
     {
-        SplitBlock();
+        float overflowAmount = MovingBlock.position.x - LastBlock.position.x;
+        float direction = overflowAmount > 0 ? 1f : -1f;
+        SplitBlock(overflowAmount, direction);
     }
 
-    void SplitBlock()
+    void SplitBlock(float overFlow, float direction)
     {
-        float overflowAmount = MovingBlock.position.x - LastBlock.position.x;
-        if (Mathf.Abs(overflowAmount) > LastBlock.localScale.x)
+        if (Mathf.Abs(overFlow) > LastBlock.localScale.x)
         {
             GameManager.Instance.EndGame();
 
             return;
         }
 
-        if (Mathf.Abs(overflowAmount) < _tolarance)
+        if (Mathf.Abs(overFlow) < _tolarance)
         {
             MovingBlock.position = new Vector3(LastBlock.position.x, LastBlock.position.y, MovingBlock.position.z);
             ClickFacade.Instance.SetCurrentBlockSize(MovingBlock.localScale.x);
             return;
         }
 
-        Debug.Log("Last object pos " + LastBlock.position);
-        Debug.Log("Moving object pos " + MovingBlock.position);
-        Debug.Log("Overflow Amount: " + overflowAmount);
-
-        float newSize = LastBlock.localScale.x - Mathf.Abs(overflowAmount);
-        float newPositionX = LastBlock.transform.position.x + (overflowAmount / 2);
+        float newSize = LastBlock.localScale.x - Mathf.Abs(overFlow);
+        float fallingBlockSize = MovingBlock.localScale.x - newSize;
+        float newPositionX = LastBlock.transform.position.x + (overFlow / 2);
 
 
         MovingBlock.localScale = new Vector3(Mathf.Abs(newSize), MovingBlock.localScale.y, MovingBlock.localScale.z);
         MovingBlock.position = new Vector3(newPositionX, MovingBlock.position.y, MovingBlock.position.z);
 
+        float cubeEdge = MovingBlock.position.x + (newSize / 2f * direction);
+        float fallingBlockXPosition = cubeEdge + fallingBlockSize / 2f * direction;
+        SpawnDropCube(fallingBlockXPosition, fallingBlockSize);
         ClickFacade.Instance.SetCurrentBlockSize(MovingBlock.localScale.x);
         if (MovingBlock.localScale.x < 0.2f)
         {
             GameManager.Instance.EndGame();
         }
+    }
+
+    void SpawnDropCube(float fallingBlockXPosition, float fallingBlockSize)
+    {
+        var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube.transform.localScale = new Vector3(fallingBlockSize, MovingBlock.localScale.y, MovingBlock.localScale.z);
+        cube.transform.position = new Vector3(fallingBlockXPosition, MovingBlock.position.y, MovingBlock.position.z);
+        cube.AddComponent<Rigidbody>();
     }
 }
